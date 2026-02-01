a26dd9fadd5323c0be2b85105d3c3cce3ecf3b27
Joseph Awuah <joawuah100@gmail.com>
2026-02-01 14:25:59 +0000

    instead of the open ai logo link use this link https://res.cloudinary.co

--- src/components/landing/logo-cloud.tsx
+++ src/components/landing/logo-cloud.tsx
@@ -6,7 +6,7 @@ export default function LogoCloud() {
           Tracking the leading AI labs
         </h2>
         <div className="mt-10 flex flex-wrap items-center justify-start gap-x-12 gap-y-8">
-          <img src="https://res.cloudinary.com/djrdh7thl/image/upload/v1769947936/Untitled_3_f6vp7t.png" alt="OpenAI Logo" className="h-10" />
+          <img src="https://res.cloudinary.com/djrdh7thl/image/upload/v1769955877/Logo_veamgf.png" alt="OpenAI Logo" className="h-10" />
           <img src="https://res.cloudinary.com/djrdh7thl/image/upload/v1769947988/Anthropic_Logo_1_r5jxzt.png" alt="Anthropic Logo" className="h-9" />
           <img src="https://res.cloudinary.com/djrdh7thl/image/upload/v1769947962/DeepMind_idlHaUh9oK_1_kb4fr4.png" alt="DeepMind Logo" className="h-10" />
         </div>
