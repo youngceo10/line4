aa148e9fd4c2a0eaca1c3f2bedf05e0ea1e135ea
Joseph Awuah <joawuah100@gmail.com>
2026-02-01 15:32:38 +0000

    replace the other image with this https://res.cloudinary.com/djrdh7thl/i

--- src/components/landing/product-feature.tsx
+++ src/components/landing/product-feature.tsx
@@ -16,7 +16,7 @@ export default function ProductFeature() {
           </div>
           <div className="flex justify-center">
               <Image
-                src="https://images.unsplash.com/photo-1618063280149-c9e9a1178a7c?w=1200&h=900&fit=crop"
+                src="https://res.cloudinary.com/djrdh7thl/image/upload/v1769959880/Screenshot_2026-02-01_183103_um8fs5.png"
                 alt="A screenshot of the Line⁴ product showing risk analysis."
                 width={600}
                 height={450}
